8ab01cd098d015ce39226cb65a1ca7b276096474
LilianAndradedasilva <lilian.andrade03@gmail.com>
2023-02-16 21:15:00 -0300

    Atualizações das aulas

--- Ebac-CY-10/cypress/e2e/endereco.cy.js
+++ Ebac-CY-10/cypress/e2e/endereco.cy.js
@@ -1,7 +1,8 @@
 /// <reference types = "cypress" />
 
-//Importando aqui o arquivo endereco.page.js
-//import EnderecoPage from '../support/page-objects/endereco.page'
+//Importando arquivos
+import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -10,8 +11,27 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
         });
     
     it ('Deve fazer cadastro de faturamento com sucesso', () => {
+        EnderecoPage.editarEnderecoFaturamento('Marcia','Souza', 'ACORD','Brasil','Rua Clélia','10100','Lapa','São Paulo','01000000','119888888888','maria@acord.com.br' )
+        cy.get('.woocommerce-message').should('contain' , 'Endereço alterado com sucesso')
         
-        //cadastro de endereço
-        //EnderecoPage.editarEnderecoFaturamento()
+    });
+
+    it ('Deve fazer cadastro de faturamento com sucesso, usando arquivo de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[2].nome,
+            dadosEndereco[2].sobrenome,
+            dadosEndereco[2].empresa,
+            dadosEndereco[2].pais,
+            dadosEndereco[2].endereco,
+            dadosEndereco[2].numero,
+            dadosEndereco[2].cidade,
+            dadosEndereco[2].estado,
+            dadosEndereco[2].cep,
+            dadosEndereco[2].telefone,
+            dadosEndereco[2].email  
+        )
+       
+        cy.get('.woocommerce-message').should('contain' , 'Endereço alterado com sucesso')
+        
     });
 });
